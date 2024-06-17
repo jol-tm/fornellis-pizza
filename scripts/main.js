cb7c3a40a31846781f3898dcad36f4db97b78327
jol-tm <joaolucastmagalhaes@gmail.com>
2024-06-17 11:52:41 -0300

    Criada animação do cardápio e feitas várias otimizações

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
     $('#open-nav').on('click', toogleMenuIcon);
     $('#close-nav').on('click', toogleMenuIcon);
     $('.menu-nav-btns').on('click', changeMenu);
+    $('.menu-card').on('mouseenter', showDescriptor);
+    $('.menu-card').on('mouseleave', hideDescriptor);
 });
 
 function checkscroll() {
     var btn = $('#scroll-up-btn');
-    if (window.scrollY > 500 && window.scrollY < 3100) {
+    if (window.scrollY > 500 && window.scrollY < 4100) {
         btn.css('scale', 1);
     } else {
         btn.css('scale', 0);
@@ -41,19 +43,27 @@ function changeMenu() {
     var beb = '<div class="col-xl-4 col-md-6"><div class="menu-card"><img class="img-fluid" src="imgs/marguerita.jpg" alt=""><h4>Bebida</h4></div></div><div class="col-xl-4 col-md-6"><div class="menu-card"><img class="img-fluid" src="imgs/marguerita.jpg" alt=""><h4>Bebida</h4></div></div><div class="col-xl-4 col-md-6"><div class="menu-card"><img class="img-fluid" src="imgs/marguerita.jpg" alt=""><h4>Bebida</h4></div></div><div class="col-xl-4 col-md-6"><div class="menu-card"><img class="img-fluid" src="imgs/marguerita.jpg" alt=""><h4>Bebida</h4></div></div><div class="col-xl-4 col-md-6"><div class="menu-card"><img class="img-fluid" src="imgs/marguerita.jpg" alt=""><h4>Bebida</h4></div></div><div class="col-xl-4 col-md-6"><div class="menu-card"><img class="img-fluid" src="imgs/marguerita.jpg" alt=""><h4>Bebida</h4></div></div></div>';
 
     if ($(this).attr('id') == 'Salgadas') {
-        $('#placeholder').css('right', '99%');
-        $('#placeholder').css('marginRight', '-6.5rem');
-        $('#placeholder').css('width', '6.5rem');   
+        $('#placeholder').css('right', '64%');
+        $('#placeholder').css('width', '35%');   
         $('#menu-cards-box').html(sal);
     } else if ($(this).attr('id') == 'Doces') {
-        $('#placeholder').css('right', '63%');
-        $('#placeholder').css('marginRight', '-5rem');
-        $('#placeholder').css('width', '5rem');
+        $('#placeholder').css('right', '35%');
+        $('#placeholder').css('width', '27%');
         $('#menu-cards-box').html(doc);
     } else if ($(this).attr('id') == 'Bebidas') {
-        $('#placeholder').css('right', '35%');
-        $('#placeholder').css('marginRight', '-6rem');
-        $('#placeholder').css('width', '6rem');
+        $('#placeholder').css('right', '1%');
+        $('#placeholder').css('width', '32%');
         $('#menu-cards-box').html(beb);
     }
+}
+
+function showDescriptor() {
+    $(this).children('.item-descriptor').css('opacity', 1);
+    $(this).children().children().css('scale', 1.1);
+    $(this).css('scale', .99); 
+}
+function hideDescriptor() {
+    $(this).children('.item-descriptor').css('opacity', 0);
+    $(this).children().children().css('scale', 1);
+    $(this).css('scale', 1);
 }
