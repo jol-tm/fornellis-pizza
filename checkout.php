96ee621467e3b4d54bef870d876622e51cce9f0f
jol-tm <joaolucastmagalhaes@gmail.com>
2024-10-04 15:11:38 -0300

    Base para o carrinho

--- checkout.php
+++ checkout.php
@@ -1,2 +1,20 @@
 <?php include "assets/header.php"; ?>
+<div class="box">
+    <h1>Carrinho</h1>
+    <div class="topo">
+        <h2>Produto</h2>
+        <h2>Quantidade</h2>
+        <h2>Preço</h2>
+    </div>
+    <div id="itens">
+        <div class="item">
+            <img class="imgProd" height="100" src="imgs\cardapio\calabresa.jpg" alt="">
+            <h3>Calabresa</h3>
+            <a href="">+</a>
+            <div class="amount">1</div>
+            <a href="">-</a>
+            <h4 class="price">R$59,99</h4>
+        </div>
+    </div>
+</div>
 <?php include "assets/footer.php"; ?>
