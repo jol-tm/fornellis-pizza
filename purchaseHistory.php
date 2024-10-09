03ea1e57102ed17e645c88386d9e9af65d36456e
jol-tm <joaolucastmagalhaes@gmail.com>
2024-10-09 11:27:07 -0300

    página de histórico

--- purchaseHistory.php
+++ purchaseHistory.php
@@ -1,2 +1,23 @@
 <?php include "assets/header.php"; ?>
+<div class="box">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Pedido</th>
+          <th scope="col">Realizado em</th>
+          <th scope="col">Valor</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider">
+        <tr>
+          <th scope="row">2x Marguerita <br> 1x Marshmellow</th>
+          <td>21-08-2024</td>
+          <td>R$179,90</td>
+          <td>Entregue</td>
+        </tr>
+        <tr>
+      </tbody>
+    </table>
+</div>
 <?php include "assets/footer.php"; ?>
