4dfb8ceec4d74ccf74f4e334e287c45d485de558
jol-tm <joaolucastmagalhaes@gmail.com>
2024-10-08 21:55:05 -0300

    teste2 commit via terminal e gedit

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "123456";
-    $dbname = "fornellis_db";
+    $dbname = "fornellis";
 
     // Cria a conexão
     $conn = new mysqli($servername, $username, $password, $dbname);
