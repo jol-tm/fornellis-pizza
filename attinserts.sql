65438eaafa536afdf37699706e5f7210b3e02829
Gabriel Fjor o  Bolado <159021308+Fjorz@users.noreply.github.com>
2024-11-05 17:24:10 -0300

    URL imagem insert
    adicionado o endereço de anexo das imagens nos inserts

---
+++ attinserts.sql
@@ -0,0 +1,36 @@
+USE fornellis;
+
+INSERT INTO produtos (nome, categoria, descricao, preco, imagem) VALUES
+('Marguerita', 'Salgada', 'Tomate, mussarela e manjericão', 59.00, 'imgs/cardapio/marguerita.jpg'),
+('Frango com Catupiry', 'Salgada', 'Frango, catupiry, milho e azeitonas', 49.99, 'imgs/cardapio/Frango-com-catupiry.jpg'),
+('Quatro Queijos', 'Salgada', 'Queijo Mussarela, Parmesão, Gorgonzola e Provolone', 59.90, 'imgs/cardapio/queijo-full.jpg'),
+('Calabresa', 'Salgada', 'Calabresa com Queijo', 47.99, 'imgs/cardapio/calabresa.jpg'),
+('Strogonoff de Frango', 'Salgada', 'Strogonoff de Frango, Queijo e Batata Palha ', 59.90, 'imgs/cardapio/strogonoff-de-frango.jpg'),
+('Pepperoni', 'Salgada', 'Pepperoni com Queijo', 49.99, 'img/cardapio/pepperoni.jpg') ;
+
+
+INSERT INTO produtos (nome, categoria, descricao, preco, imagem) VALUES
+('Morango com Nutella', 'Doce', 'Chocolate e Morangos cobertos com Leite em Pó', 49.99, 'imgs/cardapio/morango.jpeg'),
+('Chocolate com Gotas de Chocolate', 'Doce', 'Pizza doce com chocolate e gotas de chocolate', 49.90, 'imgs/cardapio/gotas-chocolate.png'),
+('Pizza com Bombom', 'Doce', 'Chocolate, Chocolate Branco, Confetes e Bombons', 49.90, 'imgs/cardapio/bombom.png'),
+('Brigadeiro com Granulado', 'Doce', 'Chocolate com Granulados, Contornados por Avelã e Cerejas', 49.90, 'imgs/cardapio/chocolate-granulado.png'),
+('Chocolate com Marshmallow', 'Doce', 'Marshmellow Tostados com Morango', 55.90, 'imgs/cardapio/Marshmello.png' ),
+('Chocolate com M&M', 'Doce', 'Chocolate com M&M e Cobertura de Morango', 55.90, 'imgs/cardapio/M&M.png');
+
+
+INSERT INTO produtos (nome, categoria, descricao, preco, imagem) VALUES
+('Sprite', 'Bebida', 'Sprite Lata 220ml', 5.00, 'imgs/cardapio/sprite.jpg'),
+('Coca-Cola', 'Bebida', 'Coca Cola KS 290ml', 7.00, 'imgs/cardapio/coca.jpg'),
+('Guaraná Antártica', 'Bebida', 'Guaraná Lata 350ml', 6.00, 'imgs/cardapio/guarana.jpg'),
+('Suco de Maracujá', 'Bebida', 'Suco Natural de Maracujá 500ml', 14.99, 'imgs/cardapio/maracuja.jpg'),
+('Suco de Laranja', 'Bebida', 'Suco Natural de Laranja 500ml', 14.99, 'imgs/cardapio/laranja.jpg'),
+('Pepsi', 'Bebida', 'Pepsi lata 269ml', 5.99, 'imgs/cardapio/pepsi.jpg');
+
+SELECT * FROM produtos;
+
+INSERT INTO administrador (email, senha) VALUES
+('admin@fornellis.com', 'admin123');
+
+SELECT * FROM PRODUTOS;
+
+
